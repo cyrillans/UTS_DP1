38b801034393cf4d6873067f7fd50c1b67f95438
cyrillans <cyrillanathania@students.usu.ac.id>
2024-11-02 06:57:50 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,31 @@
-//ketik kode jawaban noo 3 kamu disini
+uses crt;
+var
+    n, i, j: integer;
+
+begin
+    clrscr;
+    
+    writeln('Masukkan ukuran pola (angka ganjil): '); readln(n); //Input ukuran pola
+    
+    // Memeriksa apakah n adalah angka genap, jika hasil mod 2 itu 0 jadi error
+    if n mod 2 = 0 then
+    begin
+        writeln('Error: angka tidak valid, harus angka ganjil');
+    end
+    else
+    begin
+        // Membuat pola X
+        for i := 1 to n do
+        begin
+            for j := 1 to n do
+            begin
+                // Memeriksa posisi untuk mencetak bintang
+                if (i = j) or (i + j = n + 1) then
+                    write('*')
+                else
+                    write(' ');
+            end;
+            writeln; // Pindah ke baris berikutnya
+        end;
+    end;
+end.
